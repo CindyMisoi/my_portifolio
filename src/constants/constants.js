dd98381688bd4bb646b2567bf8343e6b2b127a8c
CindyMisoi <crmisoi2k@gmail.com>
2023-07-18 22:51:24 +0300

    added systemize project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'SYSTEMIZE',
+    description: "Systemize is a project management tool that allows users to keep track of teams, projects, and tasks. Systemize was developed using React, context API, and Ruby on Rails.",
+      image: '/images/Product-screenshot.png',
+      tags: ['React', 'Context', 'Ruby', 'Rails'],
+    source: 'https://github.com/CindyMisoi/Systemize',
+    visit: 'https://github.com/CindyMisoi/Systemize',
     id: 0,
   },
   {
